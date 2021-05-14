8edb7c90a5e54a71588b6d4a028fb931cd393b93
saikiran335 <saisaikiran335@gmail.com>
2021-05-14 06:52:34 +0530

    new

---
+++ areaofsquare.py
@@ -0,0 +1,21 @@
+class square:
+    def __init__(self,side):
+        self._side=side
+    @property
+    def side(self):
+        return self._side
+    @side.setter
+    def side(self,value):
+        if value>=0:
+            self._side=value
+        else:
+            print("error")
+    @property
+    def area(self):
+        return self._side ** 2
+    @classmethod
+    def unit_square(cls):
+        return cls(1)
+s = square(5)
+print(s.side)
+print(s.area)
---
+++ array example.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+a=np.array([(1,2,3),(1,2,3)])
+print(a.size)
+print(a.shape)
+print(a[0,2])
+print(a[0:,2])
+ 
